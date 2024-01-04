e1f096fedbd5b98f33c50e8d17ae0753e3ca85ab
Ante-237 <akumawahanye@gmail.com>
2024-01-04 02:49:57 +0200

    Add project files.

---
+++ Hero.cpp
@@ -0,0 +1,13 @@
+#include "Hero.h"
+
+Hero::Hero() {
+
+}
+
+Hero::~Hero() {
+
+}
+
+void Hero::init(std::string textureName, sf::Vector2f position, float mass) {
+
+}
---
+++ source.cpp
@@ -0,0 +1,113 @@
+#include "source.h"
+#include "SFML-2.6.1/include/SFML\Graphics.hpp"
+
+
+sf::Vector2f viewSize(1024, 768);
+sf::VideoMode vm(viewSize.x, viewSize.y);
+sf::RenderWindow window(vm, "Hello SFMLGame !!!", sf::Style::Default);
+
+sf::Texture skyTexture;
+sf::Sprite	skySprite;
+
+sf::Texture bgTexture;
+sf::Sprite bgSprite;
+
+sf::Vector2f playerPosition;
+bool playerMoving = false;
+
+
+void init() {
+	// Load ksy Texture
+	skyTexture.loadFromFile("Assets/graphics/sky.png");
+	// set and attacha Texture to Sprite
+	skySprite.setTexture(skyTexture);
+	bgTexture.loadFromFile("Assets/graphics/bg.png");
+	bgSprite.setTexture(bgTexture);
+
+
+}
+
+void draw() {
+	window.draw(skySprite);
+	window.draw(bgSprite);
+}
+
+void updateInput() {
+	sf::Event event;
+
+	while (window.pollEvent(event)) {
+
+		if (event.type == sf::Event::KeyPressed) {
+			if (event.key.code == sf::Keyboard::Right) {
+				playerMoving = true;
+			}
+		}
+
+		if (event.type == sf::Event::KeyReleased) {
+			if (event.key.code == sf::Keyboard::Right) {
+				playerMoving = false;
+			}
+		}
+
+		if (event.key.code == sf::Keyboard::Escape || event.type == sf::Event::Closed) {
+			window.close();
+		}
+	}
+}
+
+void update(float dt) {
+	if (playerMoving) {
+		heroSprite.move(50.0f * dt, 0);
+	}
+}
+
+
+int main() {
+	init();
+
+	sf::Clock clock;
+	//Initialize Game Objects
+	while (window.isOpen()) {
+		
+		/*
+		sf::RectangleShape rect(sf::Vector2f(500.0f, 300.0f));
+		rect.setFillColor(sf::Color::Yellow);
+		rect.setPosition(viewSize.x / 2, viewSize.y / 2);
+		rect.setOrigin(sf::Vector2f(rect.getSize().x / 2, rect.getSize().y / 2));
+
+
+		sf::CircleShape circle(100);
+		circle.setFillColor(sf::Color::Green);
+		circle.setPosition(viewSize.x / 2, viewSize.y / 2);
+		circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
+
+		sf::ConvexShape triangle;
+		triangle.setPointCount(3);
+		triangle.setPoint(0, sf::Vector2f(-100, 0));
+		triangle.setPoint(1, sf::Vector2f(0, -100));
+		triangle.setPoint(2, sf::Vector2f(100, 0));
+		triangle.setFillColor(sf::Color(128, 0, 128, 255));
+		triangle.setPosition(viewSize.x / 2, viewSize.y / 2);
+
+		window.clear(sf::Color::Blue);
+
+		window.draw(rect);
+		window.draw(circle);
+		window.draw(triangle);
+		*/
+
+
+
+		updateInput();
+		sf::Time dt = clock.restart();
+		update(dt.asSeconds());
+
+		window.clear(sf::Color::Red);
+		draw();
+		window.display();
+		// handle keyboard events
+		// update Game Objects in the scene
+		// Render Game Objects
+	}
+	return 0;
+}
